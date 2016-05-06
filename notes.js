d5296eac60d3a8be7270798678248d2c5b84b198
srini88 <pvsrinivas.ece@gmail.com>
2016-05-06 11:59:21 -0500

    html4 mockup

---
+++ notes.js
@@ -0,0 +1,103 @@
+
+make existing tags to be semantic
+
+css3 features...
+new html5 features....
+
+give consistent user experience...
+
+SSS - source , style and script...
+
+
+html5 brings in semantic tags.....
+
+canvas - very simple and rich animations...
+video -- 
+semantic tags 
+audio
+
+
+css3 - color enhancements, shadowing, rounding corners, transforms, color enhancements...
+
+
+emantics is the study of meaning, in a broader context it’s a branch of logic and linguistics.
+
+Proper semantics means a more searchable content that leads to a better search engine ranking
+
+form element was actually semangitc...
+img semantic - existed befor html5
+table is semantic...
+
+orderred and unordered lists, paragraphs, h1 - h6 heading tags all semantic..
+
+
+Non-Semantic Elements
+Non-semantic elements don’t have any special meaning, the hierchical relationships between them are merely illusory. The most widely used examples of non-semantic HTML tags are the <div></div> and the <span></span> tags.
+
+
+HTML5 introduced many new semantic elements that make easy content organization possible
+
+Probably the most popular text-level semantic tags are <strong></strong> and <em></em>, but they also existed before HTML5. Among the new inline semantic elements we can find for example the <time></time>, tag for human-readable date-times, and <mark></mark> for highlighted text.
+
+<section></section> for sections grouped around a specific theme
+<article></article> for complete or self-contained compositions such as a blog post or a widget
+<nav></nav> for navigation blocks
+<aside></aside> for complementary content such as sidebars.
+
+----------------------------------------------
+
+what is missing from HTML5 - 
+
+HTML5 < HTML4 
+
+typical html4 long doctype..... shit lot of content...
+meta tag...
+
+doctype shrunk ..
+
+<!DOCTYPE html>
+
+doctypes are required for legacy resaons, including a doctype ensures a beest effort attempt to render...when omitted browserts tend to render in an incompatible mode...
+
+then 
+<html lang="en">
+
+then 
+<meta charset = "utf-8" />
+
+The first bit in the header should be a <meta> tag that specifies the character encoding of the page. Usually, the character encoding is declared by the web server that sends the page to the browser, but many servers are not configured to send this information. Specifying it here ensures the document is displayed correctly even when it’s loaded directly from disk, without consulting a server.
+
+
+The type="text/css" attribute that was required in HTML4 is now optional in HTML5, and all current browsers know what to do if you leave the attribute out.
+The type="text/javascript" attribute, once again, is now optional in HTML5, and all current browsers behave correctly when you leave it out.
+
+
+In HTML5 both syntaxes are valid: <br> and <br />.
+In XHTML5 only <br /> is valid.
+
+
+
+why semantics --- why care... 
+
+differentiating data...
+
+
+before <div id = "nav">
+now <nav> </nav>
+
+
+<header>  <nav>   <article> <section>  <aside>   <footer>
+
+
+
+demonstrate upgrading to semantic tags...
+
+upgrade your shit to semantic tags..
+
+
+
+
+summary...
+
+
+
